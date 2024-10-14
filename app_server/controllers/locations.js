3661ad1f131bab3ca24e83c56ff8554e5571f6f0
Akshayreddyy <akshaykarasani69@gmail.com>
2024-10-14 08:19:58 +0530

    Update locations.js

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -16,7 +16,7 @@ const homelist = (req, res) => {
         distance: "100m",
       },
       {
-        name: "Cafe Hero",
+        name: "Starbucks",
         address: "125 High Street, Reading, RG6 1PS",
         rating: 4,
         facilities: ["Hot drinks", "Food", "Premium wifi"],
@@ -53,6 +53,57 @@ const locationInfo = function (req, res) {
           days: "Monday - Friday",
           opening: "7:00am",
           closing: "7:00pm",
+          closed: false,
+        },
+        {
+          days: "Saturday",
+          opening: "8:00am",
+          closing: "5:00pm",
+          closed: false,
+        },
+        {
+          days: "Sunday",
+          closed: true,
+        },
+      ],
+      reviews: [
+        {
+          author: "Simon Holmes",
+          rating: 5,
+          timestamp: "16 July 2013",
+          reviewText:
+            "What a great place. I can't say enough good things about it.",
+        },
+        {
+          author: "Charlie Chaplin",
+          rating: 3,
+          timestamp: "16 June 2013",
+          reviewText:
+            "It was okay. Coffee wasn't great, but the wifi was fast.",
+        },
+      ],
+    },
+  });
+  {
+    title: "Starcups",
+    pageHeader: { title: "Starbucks" },
+    sidebar: {
+      context:
+        "is on Loc8r because it has accessible wifi and space to sit➥down with your laptop and get some work done.",
+      callToAction:
+        "If you've been and you like it - or if you don't -➥please leave a review to help other people just like you.",
+    },
+    location: {
+      name: "Starbucks",
+      address: "High Street, Reading, RG6 1PS",
+      rating: 3,
+      facilities: ["Hot drinks", "Food", "Premium wifi"],
+      coords: { lat: 51.455041, lng: -0.9690884 },
+      openingTimes: [
+        {
+          days: "Monday - Friday",
+          opening: "8:00am",
+          closing: "8:00pm",
           closed: false,
         },
         {
